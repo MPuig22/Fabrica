abc4aeba94c30aa5a42f5ffeb02c0b3f5a71e3b6
AyaLaia <97691851+AyaLaia@users.noreply.github.com>
2023-01-16 17:39:27 +0100

    Tenemos Timer
    script +UI

--- Fabrica/Assets/Scripts/Ui/Timer.cs
+++ Fabrica/Assets/Scripts/Ui/Timer.cs
@@ -1,18 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
+    private float timeDuration = 3f * 60f;
+
+    private float timer;
+    [SerializeField] private TextMeshProUGUI firstMinute;
+    [SerializeField] private TextMeshProUGUI secondMinute;
+    [SerializeField] private TextMeshProUGUI separator;
+    [SerializeField] private TextMeshProUGUI firstSecond;
+
+    [SerializeField] private TextMeshProUGUI secondSecond;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            UpdateTimerDisplay(timer);
+        }
+        else
+        {
+            Flash();
+        }
+
+
+
+    }
+
+    private void ResetTimer()
+    {
+        timer = timeDuration;
+
+
+
+    }
+
+    private void StopTimer()
+    {
+        if (timer == 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+
+    }
+
+    private void UpdateTimerDisplay(float time)
+    {
+        float minutes = Mathf.FloorToInt(time / 60);
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        string currentTime = string.Format("{00:00}{01:00}", minutes, seconds);
+
+        firstMinute.text = currentTime[0].ToString();
+        secondMinute.text = currentTime[1].ToString();
+        firstSecond.text = currentTime[2].ToString();
+        secondSecond.text = currentTime[3].ToString();
+
+        StopTimer();
+    }
+
+    private void Flash()
+    {
+        if (timer != 0)
+        {
+            timer = 0;
+            UpdateTimerDisplay(timer);
+            StopTimer();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
