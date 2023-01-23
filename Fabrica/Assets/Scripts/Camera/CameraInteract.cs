b9dfb19f7ffefcac8b56ae24502a65fde9a575c7
MPuig22 <m.puig@santmarc.com>
2023-01-23 19:05:52 +0100

    pickups

--- Fabrica/Assets/Scripts/Camera/CameraInteract.cs
+++ Fabrica/Assets/Scripts/Camera/CameraInteract.cs
@@ -6,28 +6,53 @@ using UnityEngine;
 public class CameraInteract : MonoBehaviour
 {
     public Camera playerCamera;
+
+    //Contador de cada objeto
     public int countBrazos;
+    public int countPierna;
+    public int countPlato;
+    public int countCaja;
+    public int countPuerta;
+
     //objectos para destruir
     public GameObject destruirBrazosRobot1;
     public GameObject destruirBrazosRobot2;
+    public GameObject destruirPierna1;
+    public GameObject destruirPierna2;
+    public GameObject destruirPlato;
+    public GameObject destruirCaja;
+    public GameObject destruirPuerta;
 
     //objetos para activar
     public GameObject brazosFinal;
-
+    public GameObject piernaFinal;
+    public GameObject platoFinal;
+    public GameObject cajaFinal;
+    public GameObject puertaFinal;
 
     [Header("Pickups Variables")]
     private GameObject pickup;
     public Transform targetPoint;
+
+    //Bools para saber si se pueden agarrar los objetos
     public bool haveAPickup;
+    public bool haveAPickupPierna;
+    public bool haveAPickupPlato;
+    public bool haveAPickupCaja;
+    public bool haveAPickupPuerta;
+
     public float rayCastRange;
+
+    // layers para saber que objeto es que
     public LayerMask pickupMask;
     public LayerMask pickupMask1;
-
-    public LayerMask pantallaMicroones;
-    public LayerMask CamesRobot;
+    public LayerMask pickupPierna;
+    public LayerMask pickupPierna1;
+    public LayerMask puertaMicroones;
     public LayerMask CaixaMicroones;
-    
-    
+    public LayerMask pickupPlato;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,6 +65,30 @@ public class CameraInteract : MonoBehaviour
         if(countBrazos == 2)
         {
             brazosFinal.gameObject.SetActive(true);
+            haveAPickup = true;
+        }
+
+        if(countPierna == 2)
+        {
+            piernaFinal.gameObject.SetActive(true);
+            haveAPickupPierna = true;
+            destruirCaja.gameObject.SetActive(true);
+        }
+
+        if(countCaja == 1)
+        {
+            cajaFinal.gameObject.SetActive(true);
+            haveAPickupCaja = true;
+            destruirPlato.gameObject.SetActive(true);       
+        }
+
+        if(countPlato == 1)
+        {
+            platoFinal.gameObject.SetActive(true);
+            haveAPickupPlato = true;
+            destruirBrazosRobot2.gameObject.SetActive(true);
+            destruirBrazosRobot1.gameObject.SetActive(true);
+
         }
 
 
@@ -59,28 +108,90 @@ public class CameraInteract : MonoBehaviour
                     haveAPickup = false;
                     countBrazos++;
                     Destroy(destruirBrazosRobot1);
-
                 }
-            } 
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-
-            if (!haveAPickup)
-            {
-
-                RaycastHit hit;
-
 
                 if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, rayCastRange, pickupMask1))
                 {
                     haveAPickup = false;
                     countBrazos++;
                     Destroy(destruirBrazosRobot2);
+                }
+            } 
+        }
+
+
+        // pulsar para poder recoger las piernas
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (!haveAPickupPierna)
+            {
+
+                RaycastHit hit;
+
+
+                if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, rayCastRange, pickupPierna))
+                {
+                    haveAPickupPierna = false;
+                    countPierna++;
+                    Destroy(destruirPierna1);
+
+                }
+
+                if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, rayCastRange, pickupPierna1))
+                {
+                    haveAPickupPierna = false;
+                    countPierna++;
+                    Destroy(destruirPierna2);
+
+                }
+            } 
+        }
+
+        // pulsar para poder recoger la caja
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (!haveAPickupCaja)
+            {
+
+                RaycastHit hit;
+
+
+                if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, rayCastRange, CaixaMicroones))
+                {
+                    haveAPickupCaja = false;
+                    countCaja++;
+                    Destroy(destruirCaja);
+
+                }        
+            }
+        }
+
+        // pulsar para poder recoger la plato
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (!haveAPickupPlato)
+            {
+
+                RaycastHit hit;
+
+
+                if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, rayCastRange, pickupPlato))
+                {
+                    haveAPickupPlato = false;
+                    countPlato++;
+                    Destroy(destruirPlato);
 
                 }
             }
         }
+
     }
+
+
+    
+
+
 }
